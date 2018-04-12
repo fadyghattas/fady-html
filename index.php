07e38de395c178c635e631b9d59995eb30b4d82d
fadyghattas <fady@makhsoom.com>
2018-04-12 17:37:30 +0000

    added meta description

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
   <head>
     <title>Fady Ghattas | Resume</title>
+
+<meta name="description" content="fadresume, fady ghattas71" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="icon" href="/images/favicon.ico" >
